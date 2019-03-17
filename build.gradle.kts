b864a3dd6ba51e5ff09f38913cd10c49b9a42c04
Tobse <Tobse4Git@gmail.com>
2019-03-17 11:21:52 +0100

    🔧 Add `shadow` gradle plugin which creates a fat jar

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,18 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    java
+    application
     kotlin("jvm") version "1.3.21"
+    id("com.github.johnrengelman.shadow") version "5.0.0"
 }
 
 group = "de.tfr.tool"
 version = "1.0.5"
+
+application {
+    mainClassName = "tools.paps.RunKt"
+}
 
 repositories {
     mavenCentral()
@@ -19,7 +25,13 @@ dependencies {
 
 configure<JavaPluginConvention> {
     sourceCompatibility = JavaVersion.VERSION_1_8
+
 }
 tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
+}
+
+tasks.withType<ShadowJar> {
+    archiveBaseName.set("app")
+    minimize()
 }
