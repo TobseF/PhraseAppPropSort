938e615cc8049511b5334ac53c95d9ec7b27cd2a
Tobse <Tobse4Git@gmail.com>
2019-03-17 11:20:30 +0100

    🐛 Fix local version file loading

--- src/main/kotlin/tools/paps/Version.kt
+++ src/main/kotlin/tools/paps/Version.kt
@@ -4,11 +4,15 @@ import java.io.File
 
 
 object Version {
-    private const val filePath = "version.info"
+    private const val filePath = "/version.info"
+    private const val resources = "src/main/resources"
 
     val version: String by lazy { filePath.readTextFromFile() }
 
+    /**
+     *  Load file from `jar` or from `resources`dir. So it works locally or as packed as jar.
+     */
     private fun String.readTextFromFile(): String {
-        return javaClass.getResource(this)?.readText() ?: File(this).reader().readText()
+        return javaClass.getResource(this)?.readText() ?: File(resources + this).reader().readText()
     }
 }
